1c6c14576f1ec33d7c3f372fd06c32bb39617ead
dominick-designs <dominick@dominickdesigns.space>
2020-07-17 10:14:09 -1000

    add link to other page works now

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,8 +15,8 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-        <Link href="/posts/first-post.js"><a>Go to first post</a>
-        </Link>
+        <p>  <Link href="/posts/first-post"><a>Go to first post</a>
+        </Link></p>
       </section>
     </Layout>
   )
